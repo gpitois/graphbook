4ef9977069466d52617aaf85e181db71cffe9303
Gauthier Pitois <gauthier.pitois@gmail.com>
2019-08-02 23:03:13 -0700

    Chap 8. Add link to home page using browser history

--- src/client/components/bar/index.js
+++ src/client/components/bar/index.js
@@ -3,6 +3,7 @@ import Logout from './logout';
 import SearchBar from './search';
 import UserBar from './user';
 import { UserConsumer } from '../context/user';
+import Home from './home';
 
 export default class Bar extends Component {
   render() {
@@ -15,6 +16,7 @@ export default class Bar extends Component {
           <SearchBar />
         </div>
         <div className="buttons">
+          <Home />
           <Logout changeLoginState={this.props.changeLoginState} />
         </div>
       </div>
